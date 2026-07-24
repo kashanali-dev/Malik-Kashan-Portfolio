7ea2b39c36d2d82997b9ea59b1a4e0ec1041390d
Kashan Ali <kashanalikot@gmail.com>
2026-07-24 12:12:22 +0500

    XML

--- src/app/robots.js
+++
@@ -1,9 +0,0 @@
-export default function robots() {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://malik-kashan-portfolio.vercel.app/sitemap.xml",
-  };
-}
